7b108b27bf9104abeaee68e8c5cdc2d3df236c77
Bradford-Sledge <bradfordsledge@proton.me>
2024-08-04 14:11:28 -0500

    Created initial GameState and PlayerState

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,5 +1,6 @@
-const state = {
-  points: 0,
-};
+import { GameState as GS } from "./game";
 
-export default state;
+export { GameState } from "./game";
+export { PlayerState } from "./player";
+
+export default new GS();
